c8a9f14b9d1627999ee9d805244bfb981a875d2d
Seungwon Park <wonvely941@gmail.com>
2024-08-09 22:35:43 +0900

    1. babel, webpack practice 2. fetch statement practice

---
+++ JSPractice/esnext-project/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  // entry file
+  // https://webpack.js.org/configuration/entry-context/#entry
+  entry: ['@babel/polyfill', './src/js/main.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src/js')
+        ],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-record-and-tuple']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  mode: 'development'
+};
